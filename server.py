2bc6a519e61e99b48768261a0fadaf0fb0b7b76b
Amir Lankarani <lankaraniamir@gmail.com>
2023-04-03 19:05:55 -0400

    update all

--- server.py
+++ server.py
@@ -152,6 +152,23 @@ def genre(name):
     single=True
     )
 
+    subgenres = get_query(
+    "WITH RECURSIVE "
+    "   subgenres(sub_genre, parent_genre) AS ( "
+    "       SELECT sub_genre, parent_genre "
+    "       FROM genre_inheritance "
+    f"       WHERE parent_genre = '{name}' "
+    "       UNION "
+    "           SELECT A.sub_genre, A.parent_genre "
+    "           FROM genre_inheritance A "
+    "           INNER JOIN subgenres S ON S.sub_genre = A.parent_genre "
+    "   ) "
+    "SELECT DISTINCT sub_genre FROM subgenres "
+    "UNION SELECT DISTINCT parent_genre FROM subgenres "
+    single=True
+    )
+    print(subgenres)
+
     # context = dict(descendants = descendants)
     context = dict(children = children, parents = parents, songs = all_songs)
     return render_template("genre.html", title=name, **context)
