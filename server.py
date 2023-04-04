8376902887c3b178045d09276a04d4fbc89b145b
Amir Lankarani <lankaraniamir@gmail.com>
2023-04-04 16:00:31 -0400

    update all

--- server.py
+++ server.py
@@ -16,7 +16,6 @@ tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
-app.jinja_env.ocks = True
 
 # Creates database connecting to the given URI
 DATABASE_USERNAME = "al3625"
