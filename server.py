f788aabb8055223939166c356abfc031131348eb
Amir Lankarani <lankaraniamir@gmail.com>
2023-04-04 18:19:23 -0400

    update all

--- server.py
+++ server.py
@@ -69,7 +69,12 @@ def get_query(query, single=False, deref=False):
 
 @app.route('/songs')
 def songs():
-	songs = get_query("SELECT * FROM song ORDER BY title")
+	songs = get_query("""
+        SELECT S.title as title, A.name as artist, S.year as year
+	    FROM song S, song_credit C, artist A
+	    WHERE S.song_id = C.song_id AND C.primary_artist = True AND C.artist_id = A.artist_id
+	    ORDER BY title, artist, year)
+	""")
 	context = dict(songs = songs)
 	return render_template("songs.html", title="All Songs", **context)
 
