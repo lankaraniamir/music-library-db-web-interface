fd5aa04838b032d117fa4e7bfdb33a8cb0d9efd7
Amir Lankarani <lankaraniamir@gmail.com>
2023-04-04 14:14:38 -0400

    update all

--- server.py
+++ server.py
@@ -226,8 +226,8 @@ def user(var):
         "GROUP BY S.song_id, S.title, S.year, O.love, O.stars;"
         )
         columns = ["song","main_artists","featured_artists","other_artists","year","genres","love","stars"]
-        # references = ["song","artist","artist",None,"genre",None,None,None]
-        references = ["song",None,None,None,None,None,None,None]
+        references = ["song","artist","artist",None,"genre",None,None,None]
+        # references = ["song",None,None,None,None,None,None,None]
         # references = ["song",None,None,None,"genre",None,None,None]
         # traits = ["title","name","name",None,"name",None,None,None]
 
