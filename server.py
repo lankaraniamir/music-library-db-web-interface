5695c5da08169a353c6f5e46d248561672214aa5
Amir Lankarani <lankaraniamir@gmail.com>
2023-04-05 05:37:56 -0400

    update all

--- server.py
+++ server.py
@@ -465,7 +465,7 @@ def artist(var):
     release_columns = ["release", "release_type","primary_artist", "credits"]
     release_references = ["release", None, None, None]
 
-    return render_template('artist.html', title=var, artist=artist,,
+    return render_template('artist.html', title=var, artist=artist,
                            songs=songs, song_columns=song_columns,
                            song_references=song_references,
                            releases=releeases, release_columns=release_columns,
