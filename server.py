74dffffeadf04549ea62bffcdf9c9cdf7dec0324
Amir Lankarani <lankaraniamir@gmail.com>
2023-04-05 01:40:23 -0400

    update all

--- server.py
+++ server.py
@@ -352,12 +352,12 @@ def release(var):
     info_references = ["release","artist","artist","genre",None,None]
 
     tracks = get_query(
-        "SELECT SR.track_number as track_number, S.title as song "
+        "SELECT SR.track_number as #, S.title as song "
         "FROM release R, song_in_release SR, song S "
         f"WHERE R.title = '{sql_string(var)}' "
         "AND S.song_id = SR.song_id AND R.release_id = SR.release_id; "
     )
-    track_columns = ["track_number","song"]
+    track_columns = ["#","song"]
     track_references = [None,"song"]
 
     tracks_needed = 0
